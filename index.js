6aa03e02725793a740b2ad5a60b1df342ad571ea
RavinakS <ravinasingh697@gmail.com>
2021-08-28 14:00:49 +0530

    #@#

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const fs = require('fs');
 
 function login(){
     const username = readline.question("userName/Phone Number:- ");
@@ -12,8 +13,15 @@ function address(){
 }
 
 function food(){
-    
+    const data = fs.readFileSync('food.json');
+    const food = JSON.parse(data);
+    food.forEach(element => {
+        console.log(element.foodItems[0].foodType);
+    });
+    return typeof food;
 }
 
-console.log(login());
-console.log(address());
+// console.log(login());
+// console.log(address());
+
+food();
