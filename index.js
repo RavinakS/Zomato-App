fadc9ccd0a08938e16bae2bd2f92d4a801434fc2
RavinakS <ravinasingh697@gmail.com>
2021-08-28 16:27:55 +0530

    @@

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 const fs = require('fs');
+const { LOADIPHLPAPI } = require('dns');
 
 function login(){
     const username = readline.question("userName/Phone Number:- ");
@@ -12,13 +13,32 @@ function address(){
     return address;
 }
 
+
+function foodType(data){
+    const types_of_food = [];
+    data.forEach(element =>{
+        element.foodItems.forEach(element2=>{
+            var exist = types_of_food.includes(element2.foodType);
+            if(exist === false){
+                types_of_food.push(element2.foodType);
+            }
+        })
+    });
+
+    return types_of_food;
+
+}
+
 function food(){
     const data = fs.readFileSync('food.json');
     const food = JSON.parse(data);
-    food.forEach(element => {
-        console.log(element.foodItems[0].foodType);
-    });
-    return typeof food;
+    console.log("");
+    // food.forEach(element => {
+    //     console.log(`* ${element.restaurant}`);
+    // });
+    // console.log("");
+
+    console.log(foodType(food));
 }
 
 // console.log(login());
